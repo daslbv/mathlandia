87136ea0969625505d907adcef5619fc1e406437
Carlo <93345815+Finn331@users.noreply.github.com>
2025-05-21 19:15:46 +0700

    Timer
    disini gw menyempurnakan Timer agar bisa di pakai di setiap Stage dengan memanggil Function nya masing masing

--- Assets/scripts/Mechanism/Timer/Timer.cs
+++ Assets/scripts/Mechanism/Timer/Timer.cs
@@ -7,6 +7,7 @@ public class Timer : MonoBehaviour
     public float elapsedTime = 0f;
     public bool isRunning = false;
     public TextMeshProUGUI timerText;
+    [SerializeField] string timerLevel;
 
     void Update()
     {
@@ -14,10 +15,40 @@ public class Timer : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             UpdateTimerDisplay(elapsedTime);
+            TimerSave();
         }
     }
 
-    void UpdateTimerDisplay(float timeToDisplay)
+    void TimerSave()
+    {
+        if (timerLevel == "Level1")
+        {
+            SaveManager.instance.timerString1 = timerText.text;
+            SaveManager.instance.Save();
+        }
+        else if (timerLevel == "Level2")
+        {
+            SaveManager.instance.timerString2 = timerText.text;
+            SaveManager.instance.Save();
+        }
+        else if (timerLevel == "Level3")
+        {
+            SaveManager.instance.timerString3 = timerText.text;
+            SaveManager.instance.Save();
+        }
+        else if (timerLevel == "Level4")
+        {
+            SaveManager.instance.timerString4 = timerText.text;
+            SaveManager.instance.Save();
+        }
+        else if (timerLevel == "Level5")
+        {
+            SaveManager.instance.timerString5 = timerText.text;
+            SaveManager.instance.Save();
+        }
+    }
+
+        void UpdateTimerDisplay(float timeToDisplay)
     {
         int minutes = Mathf.FloorToInt(timeToDisplay / 60);
         int seconds = Mathf.FloorToInt(timeToDisplay % 60);
